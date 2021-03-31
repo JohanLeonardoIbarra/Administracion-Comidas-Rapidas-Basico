a32e488c0adeda27c544a14823afa4faad6dcfb8
JohanLeonardoIbarra <johanleonardois@ufps.edu.co>
2021-03-31 17:34:16 -0500

    Terminadas pestanas de configuracion y ventas

--- src/Control/VentaDeComidasR.java
+++ src/Control/VentaDeComidasR.java
@@ -10,6 +10,7 @@ import Modelo.Local;
 import Modelo.Venta;
 import java.util.LinkedList;
 import java.util.List;
+import vista.Gui;
 
 /**
  *
@@ -20,27 +21,57 @@ public class VentaDeComidasR {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Persistencia p = new Persistencia();
-        List <Venta> ventas = new LinkedList();
-        ventas.add(new Venta(1, 3, 150));
-        ventas.add(new Venta(1, 5, 200));
-        
-        List <Comidas> comidas = new LinkedList<>();
-        comidas.add(new Comidas("Hamburquesa", (double)4000));
-        comidas.add(new Comidas("Empanada", (double)800));
-        
-        //p.setVentas(ventas);
-        p.setComida(comidas);
-        p.guardarComidas();
-        //p.guardarVenta();
-        
-        //for(Venta v : p.leerVentas()){
-        //    System.out.println(v);
-        //}
-        for(Comidas c : p.leerComidas()){
-            System.out.println(c);
-        }
+    
+    private List <Local> locales;
+    private List <Comidas> comidas;
+    private List <Venta> ventas;
+    private Persistencia storage;
+
+    public VentaDeComidasR() {
+        storage = new Persistencia();
+        locales = storage.leerLocales();
+        comidas = storage.leerComidas();
+        ventas = storage.leerVentas();
     }
+   
+    public static void main(String[] args) {
+        VentaDeComidasR v = new VentaDeComidasR();
+        Gui g = new Gui(v.comidas, v.locales, v.ventas);
+        g.setVisible(true);
+    }
+
+    public List<Local> getLocales() {
+        return locales;
+    }
+
+    public void setLocales(List<Local> locales) {
+        this.locales = locales;
+    }
+
+    public List<Comidas> getComidas() {
+        return comidas;
+    }
+
+    public void setComidas(List<Comidas> comidas) {
+        this.comidas = comidas;
+    }
+
+    public List<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public Persistencia getStorage() {
+        return storage;
+    }
+
+    public void setStorage(Persistencia storage) {
+        this.storage = storage;
+    }
+    
+    
     
 }
